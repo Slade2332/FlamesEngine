44642956c2d06e41cada9d0c361a51b5f179eff0
Slade2332 <98616802+Slade2332@users.noreply.github.com>
2024-02-12 22:46:15 -0600

    New Features
    Se agregaron DepthStencilViewn DeviceContext, SwapChain, Texture y el DepthStencilView

---
+++ FlamesEngine/include/DeviceContext.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "Prerequisites.h"
+
+class DeviceContext
+{
+public:
+	DeviceContext() = default;
+	~DeviceContext() { SAFE_RELEASE(m_deviceContext); };
+
+	void
+		init();
+
+	void
+		update();
+
+	void
+		render();
+
+	void
+		destroy();
+
+	void ClearDepthStencilView(ID3D11DepthStencilView* pDepthStencilView,
+		unsigned int ClearFlags,
+		float Depth,
+		unsigned int Stencil);
+
+	void PSSetShaderResources(unsigned int StartSlot,
+		unsigned int NumViews,
+		ID3D11ShaderResourceView* const* ppShaderResourceViews);
+
+public:
+	ID3D11DeviceContext* m_deviceContext = nullptr;
+};
